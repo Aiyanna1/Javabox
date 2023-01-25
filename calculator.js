15e541b8e39c4a5477a5e4ba77ff9b54962942ba
Aiyanna1 <121715056+Aiyanna1@users.noreply.github.com>
2023-01-25 21:24:20 +0200

    calculator class work

---
+++ calculator.js
@@ -0,0 +1,12 @@
+let a = 90;
+let b = 22;
+
+if (operation == '+'){
+    console.log (a + b);
+} else if (operation == '-'){
+    console.log (a - b);
+} else if (operation == '*'); {
+    console.log (a * b); 
+} else if (operation == '/'); {
+    console.log (a / b);
+}
