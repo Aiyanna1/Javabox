fbfa967c6616c49d917238fa66ecb3d61f85cc2f
Aiyanna1 <121715056+Aiyanna1@users.noreply.github.com>
2023-01-25 13:08:30 +0200

    cylinder

---
+++ cylinder.js
@@ -0,0 +1,13 @@
+let a = 10
+let b = 7
+//a is the height and 20 is the radius
+
+let c = b*b
+//c is the radius squared
+
+let d = c*b
+//radius squared times height
+
+let e = Math.PI * d
+
+console.log (e)
