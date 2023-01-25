fa227e9705c537d6a7b857464fa1e7ddca1abf93
Aiyanna1 <121715056+Aiyanna1@users.noreply.github.com>
2023-01-25 18:56:29 +0200

    Palindrome task

---
+++ palindrome.js
@@ -0,0 +1,11 @@
+let word = 'Annery'
+let backwards = word.split("").reverse().join("");
+
+if (backwards==word) {
+    console.log('This word is a Palindrome')
+    }
+  else {
+    console.log('This word is not a Palindrome')
+}
+
+ 
