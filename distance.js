638055cf1da9f28a66b8bf707c7174376d938c69
Aiyanna1 <121715056+Aiyanna1@users.noreply.github.com>
2023-01-25 14:11:43 +0200

    distance task

---
+++ distance.js
@@ -0,0 +1,20 @@
+let x = 4;
+let y = 3;
+
+let a = x*2;
+let b = x*1;
+let c = a - b;
+
+let d =y*2;
+let e = y*1;
+let f = d - e;
+
+let g = c*c;
+let h = f*f;
+
+let i = g + h;
+let j = Math.sqrt(i);
+
+console.log(j);
+
+
